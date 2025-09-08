e406979b14ecf761af5fa112d4c3c785b61a1f71
sirconscious <mehdielbakouri0@gmail.com>
2025-09-08 13:29:56 +0100

    updating the routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
